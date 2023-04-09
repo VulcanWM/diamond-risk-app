8054120099cd500045f6f770b97b11befb9cd933
VulcanWM <logmedha09@gmail.com>
2023-04-09 19:58:47 +0100

    fixed a robber name bug

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -45,7 +45,7 @@ export default function GameScreen() {
       } else {
         var randomName = "Robber" + Math.floor(10000 + Math.random() * 90000).toString()
         saveData("name", randomName)
-        setName(name)
+        setName(randomName)
       }
     }
     fetchScoreData()
